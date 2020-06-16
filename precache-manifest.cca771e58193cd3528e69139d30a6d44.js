682d53dbd74c35366a10f0a5530896e646efe9b5
suraj kamal.shaik <surajsheru@gmail.com>
2020-06-16 10:49:09 +0530

    Updates

---
+++ precache-manifest.cca771e58193cd3528e69139d30a6d44.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84e8c01679e43a0ba284fd5d98eda4a",
+    "url": "/https//surajsheru.github.io/pten/index.html"
+  },
+  {
+    "revision": "286e687ffd0c3b75a895",
+    "url": "/https//surajsheru.github.io/pten/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e8bc633e9dd126216943",
+    "url": "/https//surajsheru.github.io/pten/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "286e687ffd0c3b75a895",
+    "url": "/https//surajsheru.github.io/pten/static/js/2.ea163fad.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/https//surajsheru.github.io/pten/static/js/2.ea163fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8bc633e9dd126216943",
+    "url": "/https//surajsheru.github.io/pten/static/js/main.c5d57637.chunk.js"
+  },
+  {
+    "revision": "45e25f29846491b8fefe",
+    "url": "/https//surajsheru.github.io/pten/static/js/runtime-main.251e240d.js"
+  },
+  {
+    "revision": "c2eee7e2fc67259a8a8384c2e97b75b9",
+    "url": "/https//surajsheru.github.io/pten/static/media/T1.c2eee7e2.jpg"
+  },
+  {
+    "revision": "88056c83d2644ff92b9c2718a6f3b698",
+    "url": "/https//surajsheru.github.io/pten/static/media/T3.88056c83.jpg"
+  },
+  {
+    "revision": "19308b1f2b87d2e410be4131d67dfb00",
+    "url": "/https//surajsheru.github.io/pten/static/media/T4.19308b1f.jpg"
+  }
+]);
